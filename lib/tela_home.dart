7818914382970bb3bed02c1a21ac3c76f4ab7c13
JeffHenriqueM <jeffhenrique@outlook.com>
2020-07-02 09:45:41 -0400

    Adicionando listView e Info Usuario

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:megaponto_oficial/main.dart';
 import 'package:intl/intl.dart';
-import 'package:true_time/true_time.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,7 +9,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   bool iniciado = false;
 
@@ -20,35 +18,114 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.green,
       key: _scaffoldKey,
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
+          child: Column(children: <Widget>[
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 10, 30, 1),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+
+                children: [
+                  Text(
+                    "Rick Sanchez",
+                    style: GoogleFonts.salsa(
+                        textStyle:
+                            TextStyle(color: Colors.white, fontSize: 20)),
+                  ),
+                  Text(
+                    "Megariano Regular",
+                    style: GoogleFonts.salsa(
+                        textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    )),
+                  ),
+                ],
+              ),
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage("images/Rubio_Circle.png")
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
           Container(
+            padding: EdgeInsets.fromLTRB(16, 40, 20, 1),
+            height: 70,
+            child: _listFuncionalidades()),
+
+        Divider(
+          height: 200,
+        ),
+        Container(
           child: RaisedButton(
             onPressed: () {
-                setState(() {
-                  iniciado = !iniciado;
-                });
-                  
-                  String time = DateFormat.yMd().add_jm().format(
-                    DateTime.now().toUtc()
-                    );
-                  final snackBar = new SnackBar(
-                  content: Text(time),
-                  duration: Duration(seconds: 5),
-                  );
+              setState(() {
+                iniciado = !iniciado;
+              });
 
-                  _scaffoldKey.currentState.showSnackBar(snackBar);
-      
+              String time =
+                  DateFormat.yMd().add_jm().format(DateTime.now().toUtc());
+              final snackBar = new SnackBar(
+                content: Text(time),
+                duration: Duration(seconds: 5),
+              );
+
+              _scaffoldKey.currentState.showSnackBar(snackBar);
             },
-            child: Text(iniciado ? 'Fechar Plantão' : 'Iniciar Plantão', 
-            style: TextStyle(fontSize:20)),
+            child: Text(iniciado ? 'Fechar Plantão' : 'Iniciar Plantão',
+                style: TextStyle(fontSize: 20)),
           ),
-        ),])
-      ),
+        ),
+      ])),
     );
   }
 
-
-  
-  
-} 
+  Widget _listFuncionalidades() {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        FlatButton(
+          child: Text(
+            "Feed",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {},
+        ),
+        FlatButton(
+          child: Text(
+            "MegaPonto",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {},
+        ),
+        FlatButton(
+          child: Text(
+            "Leaderboard",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {},
+        ),
+        FlatButton(
+          child: Text(
+            "Plantão Amigo",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
