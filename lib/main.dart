e70bb153bffc85f043beb7cc87136c063dcb4355
JeffHenriqueM <jeffhenrique@outlook.com>
2020-07-02 08:34:03 -0400

    Obtendo a hora  atual

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class Login extends StatefulWidget {
   _LoginState createState() => _LoginState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginState extends State<Login> { 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:megaponto_oficial/main.dart';
+import 'package:intl/intl.dart';
+import 'package:true_time/true_time.dart';
+
 
 class Home extends StatefulWidget {
   @override
@@ -7,12 +10,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  bool iniciado = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green,
+      key: _scaffoldKey,
       body: SafeArea(
-        child: Container(),
+        child: Column(
+          children: <Widget>[
+          Container(
+          child: RaisedButton(
+            onPressed: () {
+                setState(() {
+                  iniciado = !iniciado;
+                });
+                  
+                  String time = DateFormat.yMd().add_jm().format(
+                    DateTime.now().toUtc()
+                    );
+                  final snackBar = new SnackBar(
+                  content: Text(time),
+                  duration: Duration(seconds: 5),
+                  );
+
+                  _scaffoldKey.currentState.showSnackBar(snackBar);
+      
+            },
+            child: Text(iniciado ? 'Fechar Plantão' : 'Iniciar Plantão', 
+            style: TextStyle(fontSize:20)),
+          ),
+        ),])
       ),
     );
   }
-}
+
+
+  
+  
+} 
