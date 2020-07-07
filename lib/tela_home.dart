d888868c2ea566bf33489378b93b6e08b1871046
victor <saitovictor36@gmail.com>
2020-07-07 16:03:21 -0400

    cores

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:megaponto_oficial/main.dart';
 import 'package:intl/intl.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:megaponto_oficial/presentation/custom_icons_icons.dart'; 
+import 'package:megaponto_oficial/presentation/custom_icons_icons.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
@@ -21,14 +21,13 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     _start();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomPadding: false,
-        backgroundColor: Color.fromRGBO(74, 39, 146, 1),
+        //backgroundColor: Color.fromRGBO(74, 39, 146, 1),
         bottomNavigationBar: _bottomApp(),
         key: _scaffoldKey,
         body: SafeArea(
@@ -38,9 +37,9 @@ class _HomeState extends State<Home> {
                       begin: Alignment.topLeft,
                       end: Alignment.centerRight,
                       colors: [
-                        Color.fromRGBO(65, 3, 76, 1),
-                        Color.fromRGBO(199, 59, 34, 1)
-                      ])),
+                    Color.fromRGBO(65, 3, 76, 1),
+                    Color.fromRGBO(199, 59, 34, 1)
+                  ])),
               child: Column(children: <Widget>[
                 Container(
                   padding: EdgeInsets.fromLTRB(0, 10, 30, 1),
@@ -56,7 +55,7 @@ class _HomeState extends State<Home> {
                   child: Container(
                     decoration: BoxDecoration(
                       borderRadius:
-                      BorderRadius.only(topLeft: Radius.circular(100.0)),
+                          BorderRadius.only(topLeft: Radius.circular(100.0)),
                       color: Colors.white,
                     ),
                     child: Column(
@@ -69,6 +68,10 @@ class _HomeState extends State<Home> {
                         ),
                         Container(
                             padding: EdgeInsets.fromLTRB(50, 50, 0, 0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(100.0)),
+                            ),
                             child: _clima()),
                         Expanded(child: _plantao())
                       ],
@@ -97,29 +100,27 @@ class _HomeState extends State<Home> {
                 "Megariano Regular",
                 style: GoogleFonts.salsa(
                     textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                    )),
+                  color: Colors.white,
+                  fontSize: 15,
+                )),
               ),
             ],
           ),
         ),
-
         Container(
           width: 60,
           height: 60,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             image:
-            DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
+                DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
           ),
         )
       ],
     );
   }
 
-
-Widget _plantao() {
+  Widget _plantao() {
     return Center(
       child: Column(
         children: [
@@ -142,16 +143,26 @@ Widget _plantao() {
             color: Colors.transparent,
           ),
           Container(
-            width: 200,
-            height: 51,
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(143, 38, 56, 1),
-                borderRadius: BorderRadius.all(Radius.circular(24))),
-            child: loading ? FlatButton(child: CircularProgressIndicator(),)
-            : started ?
-            FlatButton(child: Text('Fechar Plantão'), onPressed: () async => _fecharPlantao(),)
-            : FlatButton(child: Text('Iniciar Plantão'), onPressed: () async => _iniciarPlantao(),)
-          ),
+              width: 200,
+              height: 51,
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(199, 59, 34, 1),
+                  borderRadius: BorderRadius.all(Radius.circular(24))),
+              child: loading
+                  ? FlatButton(
+                      child: CircularProgressIndicator(),
+                    )
+                  : started
+                      ? FlatButton(
+                          child: Text('Fechar Plantão',
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () async => _fecharPlantao(),
+                        )
+                      : FlatButton(
+                          child: Text('Iniciar Plantão',
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () async => _iniciarPlantao(),
+                        )),
         ],
       ),
     );
@@ -171,7 +182,6 @@ Widget _plantao() {
               image: AssetImage("images/Rubio_Circle.png"),
             ),
           ),
-
         ),
         Container(
           padding: EdgeInsets.only(left: 20),
@@ -180,29 +190,7 @@ Widget _plantao() {
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             image:
-            DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
-          ),
-
-        ),
-        Container(
-          padding: EdgeInsets.only(left: 20),
-          width: 80,
-          height: 80,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image:
-            DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
-          ),
-
-        ),
-        Container(
-          padding: EdgeInsets.only(left: 20),
-          width: 80,
-          height: 80,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image:
-            DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
+                DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
           ),
         ),
         Container(
@@ -212,7 +200,7 @@ Widget _plantao() {
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             image:
-            DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
+                DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
           ),
         ),
         Container(
@@ -222,7 +210,7 @@ Widget _plantao() {
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             image:
-            DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
+                DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
           ),
         ),
         Container(
@@ -232,7 +220,27 @@ Widget _plantao() {
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             image:
-            DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
+                DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 20),
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image:
+                DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 20),
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image:
+                DecorationImage(image: AssetImage("images/Rubio_Circle.png")),
           ),
         ),
       ],
@@ -240,43 +248,62 @@ Widget _plantao() {
   }
 
   Widget _bottomApp() {
-    return BottomAppBar(
-      shape: CircularNotchedRectangle(),
-      color: Color.fromRGBO(74, 39, 146, 1),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.home,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.list,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.map,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-        ],
-      ),
-    );
+    return BottomNavigationBar(
+        backgroundColor: Color.fromRGBO(199, 59, 34, 1),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(CustomIcons.feed), title: Text('Feed')),
+          BottomNavigationBarItem(
+              icon: Icon(
+                CustomIcons.megaponto,
+              ),
+              title: Text('MegaPonto')),
+          BottomNavigationBarItem(
+              icon: Icon(CustomIcons.leaderboard), title: Text('Leaderboard')),
+          BottomNavigationBarItem(
+              icon: Icon(CustomIcons.plantaoamigo),
+              title: Text('Plantão Amigo'))
+        ]
+
+        // shape: CircularNotchedRectangle(),
+        // color: Color.fromRGBO(199, 59, 34, 1),
+        // child: Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //   children: <Widget>[
+        //     IconButton(
+        //       icon: Icon(
+        //         CustomIcons.feed,
+        //         color: Colors.white,
+        //       ),
+        //       onPressed: () {},
+        //     ),
+        //     IconButton(
+        //       icon: Icon(
+        //         CustomIcons.megaponto,
+        //         color: Colors.white,
+        //       ),
+        //       onPressed: () {},
+        //     ),
+        //     IconButton(
+        //       icon: Icon(
+        //         CustomIcons.leaderboard,
+        //         color: Colors.white,
+        //       ),
+        //       onPressed: () {},
+        //     ),
+        //     IconButton(
+        //       icon: Icon(
+        //         CustomIcons.plantaoamigo,
+        //         color: Colors.white,
+        //       ),
+        //       onPressed: () {},
+        //     ),
+        //   ],
+        // ),
+        );
   }
 
   Widget _membrosOnline() {
@@ -353,34 +380,30 @@ Widget _plantao() {
   }
 
   void _iniciarPlantao() async {
-
     SharedPreferences prefs = await _getSharedInstance();
 
-    prefs.setInt('startTime', DateTime.now().toUtc().millisecondsSinceEpoch).then((value) {
+    prefs
+        .setInt('startTime', DateTime.now().toUtc().millisecondsSinceEpoch)
+        .then((value) {
       setState(() => started = true);
       _showSnack(DateFormat.Hm().format(DateTime.now()), true);
     });
   }
 
   void _fecharPlantao() async {
-
     SharedPreferences prefs = await _getSharedInstance();
 
     int time = prefs.get('startTime');
     DateTime startTime = DateTime.fromMillisecondsSinceEpoch(time);
     Duration timeOnline = DateTime.now().toUtc().difference(startTime);
 
-
-
     prefs.remove('startTime').then((value) {
       setState(() => started = false);
       _showSnack(_formatDuration(timeOnline), false);
     });
-
   }
 
-  String _formatDuration(Duration duration){
-
+  String _formatDuration(Duration duration) {
     String twoDigits(int n) => n.toString().padLeft(2, "0");
     String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
     String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
@@ -402,33 +425,30 @@ Widget _plantao() {
     });
     print(prefs);
     return prefs;
-
   }
 
-  void _showSnack(String time, bool start){
-
+  void _showSnack(String time, bool start) {
     SnackBar snackBar;
 
-    snackBar = start ? snackBar = new SnackBar(
-      content: Text('Plantão iniciado às $time'),
-      duration: Duration(seconds: 2),
-    )
+    snackBar = start
+        ? snackBar = new SnackBar(
+            content: Text('Plantão iniciado às $time'),
+            duration: Duration(seconds: 2),
+          )
         : snackBar = new SnackBar(
-      content: Text('Duração do plantão: $time'),
-      duration: Duration(seconds: 2),
-    );
+            content: Text('Duração do plantão: $time'),
+            duration: Duration(seconds: 2),
+          );
 
     _scaffoldKey.currentState.showSnackBar(snackBar);
   }
 
-  void _start() async{
-
+  void _start() async {
     await _getSharedInstance().then((value) {
       setState(() {
         started = value.get('startTime') != null;
         loading = false;
       });
     });
-
   }
 }
