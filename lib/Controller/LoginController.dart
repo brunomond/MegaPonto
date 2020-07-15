45fa0a69bb3f3216977c32493cd2a22397794306
Jefferson <jeffhenrique@outlook.com>
2020-07-15 15:07:28 -0400

    teste de login

--- lib/Controller/LoginController.dart
+++ lib/Controller/LoginController.dart
@@ -8,35 +8,27 @@ import 'package:shared_preferences/shared_preferences.dart';
 const String URL_LOGIN = 'https://paineljunior.com.br/api/login';
 
 class LoginController {
-
   String senha;
   LoginController({this.senha});
   final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
 
-
   Future<bool> doLogin(Usuario usuario) async {
+    if (!formKey.currentState.validate()) return false;
 
-    if(!formKey.currentState.validate()) return false;
+    formKey.currentState.save();
 
-  formKey.currentState.save();
+    Map<String, dynamic> body = {'email': usuario.email, 'senha': senha};
 
-   Map<String, dynamic> body = {
-     'email' : usuario.email,
-     'senha' : senha
-   };
-
-    http.Response response = await http.post(URL_LOGIN, body: json.encode(body));
+    http.Response response = await http.post(URL_LOGIN, body: body);
 
     Map parsedJson = json.decode(response.body);
 
-    if(parsedJson['error'] != null) return false;
+    if (parsedJson['error'] != null) return false;
 
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString(parsedJson['token'], response.body);
     prefs.setString('loginAuth', parsedJson['token']);
 
     return prefs.get('loginAuth') != null;
-
- }
-
+  }
 }
