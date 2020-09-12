ab8a32d5362bb6d5056839d60f3b11456192356e
victor <saitovictor36@gmail.com>
2020-09-12 17:08:37 -0400

    inicio crud

--- lib/View/AdmPages/tela_edit_funcionario.dart
+++ lib/View/AdmPages/tela_edit_funcionario.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:megaponto_oficial/View/HomePage/Widgets/MembrosCard.dart';
+import 'package:megaponto_oficial/View/AdmPages/WidgetsAdm/MembrosCrud.dart';
 import 'package:megaponto_oficial/View/Utils/GradientAppBar.dart';
-import 'package:megaponto_oficial/View/HomePage/Widgets/BottomApp.dart';
 import 'package:megaponto_oficial/Model/usuario.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:megaponto_oficial/Controller/plantao_amigo_controller.dart';
+import 'package:megaponto_oficial/Resources/Globals.dart';
+import 'package:megaponto_oficial/View/Utils/Loading.dart';
 
 class EditarFuncionario extends StatefulWidget {
   final GlobalKey<ScaffoldState> scaffold;
@@ -14,8 +17,40 @@ class EditarFuncionario extends StatefulWidget {
 }
 
 class _EditarFuncionarioState extends State<EditarFuncionario> {
+  PlantaoAmigoController controller = PlantaoAmigoController();
+
   @override
   Widget build(BuildContext context) {
-    return Text('bla bla');
+    return Scaffold(
+      appBar: GradientAppBar(
+        text: 'Administrador',
+      ),
+      body: Observer(
+        builder: (_) {
+          if (controller.loading || controller.membrosEjOutput.data.length == 0)
+            return SizedBox(
+                height: Globals.windowSize.height,
+                width: Globals.windowSize.width,
+                child: Loading());
+
+          List<Usuario> membrosEj = controller.membrosEjOutput.data;
+
+          return RefreshIndicator(
+              onRefresh: controller.fetchData,
+              child: controller.loadingNewState
+                  ? SizedBox(
+                      height: Globals.windowSize.height,
+                      width: Globals.windowSize.width,
+                      child: Loading())
+                  : ListView.builder(
+                      itemBuilder: (_, index) => MembrosCrud(
+                        lista: membrosEj,
+                        index: index,
+                      ),
+                      itemCount: membrosEj.length,
+                    ));
+        },
+      ),
+    );
   }
 }
